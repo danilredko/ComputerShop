bbe5cb46a998724f0eb05f224fa904d41c622538
Danil <dredko@outlook.com>
2019-09-11 15:26:03 -0400

    Fixed part 1 and part2

--- Assignment3/static/js/slideshowpart2.js
+++ Assignment3/static/js/slideshowpart2.js
@@ -1,11 +1,68 @@
 
 
-var slideIndex = 0;
-showSlides();
 
+function BuildSlideShow(){
+
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+      var myObj = JSON.parse(this.responseText);
+
+        console.log(myObj);
+
+        list_images = myObj.images;
+
+        total_images = list_images.length
+
+        for (i = 0; i < total_images ; i++) {
+
+
+            var div_img = document.createElement('div');
+            div_img.setAttribute('class', 'mySlides fade');
+            div_img.setAttribute('id', 'img'+i);
+            document.getElementById("slideshow").appendChild(div_img);
+
+            console.log('image is set');
+
+            n = i + 1;
+            var numbertext_div = document.createElement('div');
+            numbertext_div.setAttribute('class', 'numbertext');
+            numbertext_div.textContent = n + ' / ' + total_images;
+            document.getElementById('img'+i).appendChild(numbertext_div);
+
+            var img_el = document.createElement('img');
+            img_el.setAttribute('src', list_images[i].path );
+            img_el.setAttribute('style', 'width:100%');
+            document.getElementById('img'+i).appendChild(img_el);
+
+
+            var caption_el = document.createElement('div');
+            caption_el.setAttribute('class', 'text');
+            caption_el.textContent = list_images[i].caption;
+            document.getElementById('img'+i).appendChild(caption_el);
+
+            var span_el = document.createElement('span');
+            span_el.setAttribute('class', 'dot');
+            document.getElementById('dots_for_control').appendChild(span_el);
+
+        }
+    }
+
+  };
+
+  xmlhttp.open("GET", "http://127.0.0.1:8000/static/json_files/all_images.json", true);
+  xmlhttp.send();
+
+};
+
+var slideIndex = 0;
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  console.log(slides.length);
+  console.log(document.getElementById('img1'));
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -15,7 +72,10 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
 
-  setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 2000);
 }
 
 
+BuildSlideShow();
+
+setTimeout(showSlides, 100);
